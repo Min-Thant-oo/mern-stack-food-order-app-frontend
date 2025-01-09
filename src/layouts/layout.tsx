c6515a123bbca1fddb939025b471b29d989b4bf5
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-01-09 15:42:26 -0600

    DesktopNav added

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -5,10 +5,10 @@ type Props = {
 }
 const Layout = ({ children }: Props) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen  ">
         <Header />
 
-        <div className="container mx-auto flex-1 py-10">{children}</div>
+        <div className="container mx-auto flex-1 py-10 px-4 md:px-0">{children}</div>
     </div>
   )
 }
