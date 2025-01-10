4d3938a6049613aa49c2b7ea6f833afd6cca15e6
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-01-09 20:31:10 -0600

    authenticated user links added

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -5,24 +5,43 @@ import {
     SheetTitle,
     SheetTrigger,
   } from "@/components/ui/sheet"
-import { Menu } from "lucide-react"
+import { CircleUserRound, Menu } from "lucide-react"
 import { Separator } from "./ui/separator"
 import { Button } from "./ui/button"
+import { useAuth0 } from "@auth0/auth0-react"
+import MobileNavLinks from "./MobileNavLinks"
   
 
 const MobileNav = () => {
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
   return (
     <Sheet>
         <SheetTrigger>
           <Menu className="text-orange-500" />
         </SheetTrigger>
         <SheetContent className="space-y-3">
-            <SheetTitle><span> Welcome to SolarEats.com! </span></SheetTitle>
+            <SheetTitle>
+              {isAuthenticated 
+                ? <span className="flex items-center font-bold gap-2 ">
+                    <CircleUserRound className="text-orange-500" />
+                    {user?.name}
+                  </span> 
+                : <span> Welcome to SolarEats.com! </span> 
+              }
+            </SheetTitle>
             
             <Separator />
 
-            <SheetDescription className='flex'>
-                <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+            <SheetDescription className='flex flex-col gap-4'>
+                {isAuthenticated 
+                  ? <MobileNavLinks />
+                  : <Button 
+                      onClick={async () => await loginWithRedirect()} 
+                      className="flex-1 font-bold bg-orange-500"
+                    >
+                      Log In
+                    </Button>
+                }
             </SheetDescription>
         </SheetContent>
     </Sheet>
---
+++ src/components/UsernameMenu.tsx
@@ -0,0 +1,40 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { DropdownMenu, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
+import { CircleUserRound } from "lucide-react"
+import { DropdownMenuContent } from "./ui/dropdown-menu";
+import { Link } from "react-router-dom";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+
+const UsernameMenu = () => {
+    const { user, logout } = useAuth0();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-orange-500 gap-2">
+                <CircleUserRound className="text-orange-500" />
+                {user?.name}
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-[150px]">
+                <DropdownMenuItem className="">
+                    <Link to='/user-profile' className="flex justify-center font-bold hover:text-orange-500 py-1">
+                        User Profile
+                    </Link>
+                </DropdownMenuItem>
+
+                <Separator />
+
+                <DropdownMenuItem className="flex flex-1 justify-center items-center pt-2 pb-1">
+                    <Button 
+                        className=" font-bold bg-orange-500" 
+                        onClick={() => logout()}
+                    >
+                        Log Out
+                    </Button>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+export default UsernameMenu
