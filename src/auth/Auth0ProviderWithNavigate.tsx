6f4f4821575183104559ddf30deccf237a0fdff1
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-01-10 16:14:36 -0600

    created user request

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,9 +1,12 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 
 type Props = {
     children: React.ReactNode;
 }
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+
+    const { createUser } = useCreateMyUser();
 
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
@@ -14,7 +17,9 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     }
 
     const onRedirectCallback = (appState?: AppState, user?: User) => {
-        console.log('User:', user)    
+        if(user?.sub && user?.email) {
+            createUser({ auth0Id: user.sub, email: user.email });
+        }  
     }
 
     return (
