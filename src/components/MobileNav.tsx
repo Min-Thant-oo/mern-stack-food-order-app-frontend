15fe500c4dc2f910e1b0fe05ae6ecaf816bbd4e1
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-01-15 14:43:05 -0600

    change from name to email

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -24,7 +24,7 @@ const MobileNav = () => {
               {isAuthenticated 
                 ? <span className="flex items-center font-bold gap-2 ">
                     <CircleUserRound className="text-orange-500" />
-                    {user?.name}
+                    {user?.email}
                   </span> 
                 : <span> Welcome to SolarEats.com! </span> 
               }
