c56ce9f52977bc338c421b9bc29dd2015abdc9cb
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-01-14 19:34:26 -0600

    add Menu Section of manage restaurant

--- src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -5,36 +5,40 @@ import { z } from "zod";
 import DetailsSection from "./DetailsSection";
 import CuisinesSection from "./CuisinesSection";
 import { Separator } from "@/components/ui/separator";
+import MenuSection from "./MenuSection";
 
 const formSchema = z.object({
     restaurantName: z.string({
-      required_error: "restuarant name is required",
+      required_error: "Restaurant name is required",
     }),
     city: z.string({
-      required_error: "city is required",
+      required_error: "City is required",
     }),
     country: z.string({
-      required_error: "country is required",
+      required_error: "Country is required",
     }),
     deliveryPrice: z.coerce.number({
-      required_error: "delivery price is required",
-      invalid_type_error: "must be a valid number",
+      required_error: "Delivery price is required",
+      invalid_type_error: "Delivery price must be a valid number",
     }),
     estimatedDeliveryTime: z.coerce.number({
-      required_error: "estimated delivery time is required",
-      invalid_type_error: "must be a valid number",
+      required_error: "Estimated delivery time is required",
+      invalid_type_error: "Estimated delivery time must be a valid number",
     }),
     cuisines: z.array(z.string()).nonempty({
-      message: "please select at least one item",
+      message: "Please select at least one item",
     }),
     menuItems: z.array(
       z.object({
-        name: z.string().min(1, "name is required"),
-        price: z.coerce.number().min(1, "price is required"),
+        name: z.string().min(1, "Name is required"),
+        price: z.coerce.number({
+          required_error: "Price is required",
+          invalid_type_error: "Price must be a valid number",
+        }),
       })
     ),
     imageUrl: z.string().optional(),
-    imageFile: z.instanceof(File, { message: "image is required" }).optional(),
+    imageFile: z.instanceof(File, { message: "Image is required" }),
 });
 
 type restaurantFormData = z.infer<typeof formSchema>;
@@ -60,9 +64,12 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
   return (
     <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 bg-gray-50 p-10 rounded-lg">
-           <DetailsSection /> 
-           <Separator />
-           <CuisinesSection />
+          <DetailsSection /> 
+            <Separator />
+          <CuisinesSection />
+            <Separator />
+          <MenuSection />
+            <Separator />
         </form>
     </Form>
   )
