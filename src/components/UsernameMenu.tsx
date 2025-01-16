1eea98fd78693541440a119f883f737cc10db8b7
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-01-15 21:24:50 -0600

    remove ring css from dropdowntrigger and dropdownmenuitem

--- src/components/UsernameMenu.tsx
+++ src/components/UsernameMenu.tsx
@@ -19,17 +19,17 @@ const UsernameMenu = () => {
 
     return (
         <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
-            <DropdownMenuTrigger className="flex items-center px-4 py-2 rounded-md transition-colors font-bold hover:text-orange-500 gap-2">
+            <DropdownMenuTrigger className="flex items-center px-4 py-2 rounded-md transition-colors font-bold hover:text-orange-500 gap-2 focus:outline-none focus:ring-0">
                 <CircleUserRound className="text-orange-500" />
                 {user?.email}
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-64 p-2">
-                <DropdownMenuItem onClick={() => setIsOpen(false)}>
+                <DropdownMenuItem className="focus:outline-none focus:ring-0" onClick={() => setIsOpen(false)}>
                     <Link to='/manage-restaurant' className={getLinkClass("/manage-restaurant")} >
                         Manage Restaurant
                     </Link>
                 </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setIsOpen(false)}>
+                <DropdownMenuItem className="focus:outline-none focus:ring-0" onClick={() => setIsOpen(false)}>
                     <Link to='/user-profile' className={getLinkClass("/user-profile")} >
                         User Profile
                     </Link>
@@ -37,7 +37,7 @@ const UsernameMenu = () => {
 
                 <Separator className="my-2" />
 
-                <DropdownMenuItem className="flex flex-1 justify-center items-center pt-2 pb-1">
+                <DropdownMenuItem className="flex flex-1 justify-center items-center pt-2 pb-1 focus:outline-none focus:ring-0">
                     <Button 
                         className=" w-full font-bold bg-orange-500 hover:bg-orange-600 transition-colors" 
                         onClick={() => logout({
