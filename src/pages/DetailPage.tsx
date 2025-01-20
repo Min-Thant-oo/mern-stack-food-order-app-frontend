eeec5237d8355443c7770f00b0c54a8dc9088347
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-01-19 18:56:59 -0600

    add to cart

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -4,10 +4,55 @@ import { useParams } from "react-router-dom"
 import { AspectRatio } from '@/components/ui/aspect-ratio';
 import RestaurantInfo from "@/components/RestaurantInfo";
 import MenuItem from "@/components/MenuItem";
+import { useState } from "react";
+import OrderSummary from '@/components/OrderSummary';
+import { Card } from "@/components/ui/card";
+import { MenuItem as MenuItemType } from "@/types";
+
+export type CartItem = {
+    _id: string;
+    name: string;
+    price: number;
+    quantity: number;
+}
 
 const DetailPage = () => {
     const { restaurantId } = useParams();
     const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+
+    const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+    const addToCart = (menuItem: MenuItemType) => {
+        setCartItems((prevCartItems) => {
+            // 1. check if the item is already in cart
+            const existingCartItem = prevCartItems.find(
+                (cartItem) => cartItem._id === menuItem._id
+            );
+
+            let updatedCartItems;
+
+            // 2. if item is already in cart, update the quantity
+            if(existingCartItem) {
+                updatedCartItems = prevCartItems.map((cartItem) => 
+                    cartItem._id === menuItem._id 
+                        ? { ...cartItem, quantity: cartItem.quantity + 1 } 
+                        : cartItem
+                );
+            } else {
+                // 3. if item is not in cart, add it as a new item
+                updatedCartItems = [
+                    // update the object in prevCartItems 
+                    ...prevCartItems, {
+                        _id: menuItem._id,
+                        name: menuItem.name,
+                        price: menuItem.price,
+                        quantity: 1,
+                    }
+                ]
+            }
+            return updatedCartItems;
+        });
+    }
 
     if(isLoading || !restaurant) {
         return <Spinner />;
@@ -21,13 +66,23 @@ const DetailPage = () => {
                     className="rounded-md object-cover h-full w-full"
                 />
             </AspectRatio>
-            <div className="grid md:grid-cols-[4fr-2ft] gap-5 md:px-32">
+            <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32">
                 <div className="flex flex-col gap-4">
                     <RestaurantInfo restaurant={restaurant} />
                     <span className="text-2xl font-bold tracking-tight">Menu</span>
                     {restaurant.menuItems.map((menuItem) => 
-                        <MenuItem key={menuItem.name} menuItem={menuItem} />
+                        <MenuItem 
+                            key={menuItem._id} 
+                            menuItem={menuItem} 
+                            addToCart={() => addToCart(menuItem)}
+                        />
                     )}
+                </div>
+
+                <div>
+                    <Card>
+                        <OrderSummary restaurant={restaurant} cartItems={cartItems} />
+                    </Card>
                 </div>
             </div>
         </div>
