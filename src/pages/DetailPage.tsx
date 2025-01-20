a22b5a25c8721d99bca5c056243ff9d15f632216
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-01-20 04:15:11 -0600

    add session storage for cart items

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -21,7 +21,10 @@ const DetailPage = () => {
     const { restaurantId } = useParams();
     const { restaurant, isLoading } = useGetRestaurant(restaurantId);
 
-    const [cartItems, setCartItems] = useState<CartItem[]>([]);
+    const [cartItems, setCartItems] = useState<CartItem[]>(() => {
+        const storedCartItems = sessionStorage.getItem(`cartItems-${restaurantId}`);
+        return storedCartItems ? JSON.parse(storedCartItems) : [];
+    });
 
     const addToCart = (menuItem: MenuItemType) => {
         setCartItems((prevCartItems) => {
@@ -51,6 +54,8 @@ const DetailPage = () => {
                     }
                 ]
             }
+
+            sessionStorage.setItem(`cartItems-${restaurantId}`, JSON.stringify(updatedCartItems));
             return updatedCartItems;
         });
     };
@@ -60,6 +65,8 @@ const DetailPage = () => {
             const updatedCartItems = prevCartItems.filter(
                 (item) => cartItem._id !== item._id
             );
+
+            sessionStorage.setItem(`cartItems-${restaurantId}`, JSON.stringify(updatedCartItems));
     
             return updatedCartItems;
         });
