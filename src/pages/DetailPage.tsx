c0afd26007693e5144010835b399a83abedd22b4
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-01-21 20:32:52 -0600

    create stripe api request

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -10,6 +10,7 @@ import { Card, CardFooter } from "@/components/ui/card";
 import { MenuItem as MenuItemType } from "@/types";
 import CheckoutButton from "@/components/CheckoutButton";
 import { UserFormData } from "@/forms/user-profile-form/UserProfileForm";
+import { useCreateCheckoutSession } from "@/api/OrderApi";
 
 export type CartItem = {
     _id: string;
@@ -21,6 +22,7 @@ export type CartItem = {
 const DetailPage = () => {
     const { restaurantId } = useParams();
     const { restaurant, isLoading } = useGetRestaurant(restaurantId);
+    const { createCheckoutSession, isLoading: isCheckoutLoading } = useCreateCheckoutSession();
 
     const [cartItems, setCartItems] = useState<CartItem[]>(() => {
         const storedCartItems = sessionStorage.getItem(`cartItems-${restaurantId}`);
@@ -73,8 +75,29 @@ const DetailPage = () => {
         });
     };
 
-    const onCheckout = (userFormData: UserFormData) => {
-        console.log("UserFormData", userFormData);
+    const onCheckout = async (userFormData: UserFormData) => {
+        if (!restaurant) {
+          return;
+        }
+    
+        const checkoutData = {
+            cartItems: cartItems.map((cartItem) => ({
+                menuItemId: cartItem._id,
+                name: cartItem.name,
+                quantity: cartItem.quantity.toString(),
+            })),
+            restaurantId: restaurant._id,
+            deliveryDetails: {
+                name: userFormData.name,
+                addressLine1: userFormData.addressLine1,
+                city: userFormData.city,
+                country: userFormData.country,
+                email: userFormData.email as string,
+            },
+        };
+    
+        const data = await createCheckoutSession(checkoutData);
+        window.location.href = data.url;
     };
 
     if(isLoading || !restaurant) {
@@ -113,6 +136,7 @@ const DetailPage = () => {
                             <CheckoutButton 
                                 disabled={cartItems.length === 0} 
                                 onCheckout={onCheckout}
+                                isLoading={isCheckoutLoading}
                             />
                         </CardFooter>
                     </Card>
