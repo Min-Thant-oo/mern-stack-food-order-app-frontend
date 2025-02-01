62071ca28b875328572b44d306a7d9346b30b74e
Min Thant Oo <minthantoo.ardil@gmail.com>
2025-02-01 00:53:47 -0600

    serves the image from cloudinary

--- src/components/Layout/Hero.tsx
+++ src/components/Layout/Hero.tsx
@@ -1,9 +1,15 @@
-import hero from '@/assets/hero.png';
+// import hero from '@/assets/hero.png';
 
 const Hero = () => {
+
+  const hero = 'https://res.cloudinary.com/dsrc9n3iy/image/upload/v1738392562/SolarEats/n6cpqgntjljq55viqysu.png'
   return (
     <div>
-        <img src={hero} className='w-full max-h-[350px] object-cover' />
+        <img 
+          src={hero} 
+          alt="Hero Image" 
+          className='w-full max-h-[350px] object-cover' 
+        />
     </div>
   )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
-import landing from '../assets/landing.png'
-import appDownload from '../assets/appDownload.png'
+// import landing from '../assets/landing.png'
+// import appDownload from '../assets/appDownload.png'
 import { Helmet } from 'react-helmet-async'
 import SearchBar, { SearchForm } from '@/components/shared/SearchBar'
 import { useNavigate } from 'react-router-dom'
@@ -13,6 +13,9 @@ const HomePage = () => {
         pathname: `/search/${searchFormValues.searchQuery}`,
       });
     };
+
+    const landing = 'https://res.cloudinary.com/dsrc9n3iy/image/upload/v1738392562/SolarEats/zxrllusaaulumlzunzck.png';
+    const appDownload = 'https://res.cloudinary.com/dsrc9n3iy/image/upload/v1738392562/SolarEats/w4anffrn2jvaywc5rqao.png';
 
     return (
 
@@ -32,13 +35,13 @@ const HomePage = () => {
                 </div>
 
                 <div className="grid md:grid-cols-2 gap-5">
-                    <img src={landing} />
+                    <img src={landing} alt='landing' loading='lazy' />
                     <div className="flex flex-col items-center justify-center gap-4 text-center">
                         <span className="font-bold text-3xl tracking-tigher">
                             Order takeaway even faster
                         </span>
                         <span>Download the <span className='text-orange-500 font-bold'>SolarEats</span> App for faster ordering and personalised recommendations</span>
-                        <img src={appDownload} />
+                        <img src={appDownload} alt='appDownload' loading='lazy' />
                     </div>
                 </div>
             </div>
